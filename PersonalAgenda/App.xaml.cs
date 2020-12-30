a2ec7e52ec65d33f383414f75fe5b084775d21a8
Otilia Huszar <otilia.huszar@pitechnologies.ro>
2020-12-30 15:08:30 +0200

    Add project files.

---
+++ PersonalAgenda/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using PersonalAgenda.Data;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PersonalAgenda
+{
+    public partial class App : Application
+    {
+        static AgendaDatabase database;
+        public static AgendaDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new AgendaDatabase(Path.Combine(Environment.GetFolderPath(
+                        Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new AgendaPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
